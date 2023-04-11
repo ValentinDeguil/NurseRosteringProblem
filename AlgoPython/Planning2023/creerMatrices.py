a08390bb4bf630aaf30841030706924e3302d88c
valentindeguil <valentin.deguil@gmail.com>
2023-04-11 15:39:34 +0200

    .

---
+++ AlgoPython/Planning2023/creerMatrices.py
@@ -0,0 +1,24 @@
+import pandas as pd
+def creerMatrices():
+
+    cardO = 36
+    cardP = 36
+    dataOperateurs = pd.read_csv('./data2023/operateursExperimentations.csv', sep=",")
+
+    kappa = []
+    for i in range(cardO):
+        ligne = []
+        for p in range(cardP):
+            if dataOperateurs.iloc[i, p + 3] == 1:
+                ligne.append(1)
+            else:
+                ligne.append(0)
+        kappa.append(ligne)
+
+    print()
+    print("kappa = [")
+    for i in range(len(kappa)-1):
+        print(str(kappa[i]) + ",")
+    print(str(kappa[len(kappa)-1]) + "]")
+
+creerMatrices()
